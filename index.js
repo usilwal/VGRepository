864ab2942d06c0fdb44f6a0f4d1a037870b67a46
usilwal <usilwal@hotmail.com>
2021-04-27 17:43:21 -0400

    games now display with dates

--- index.js
+++ index.js
@@ -6,22 +6,40 @@ let [month, date, year] = new Date().toLocaleDateString("en-US").split("/");
 const currDate = `${year}-${month}-${date} 00:00:00`;
 
 const API_KEY = 'd2b070eb1e16be91e89182b57fe7ae25563c0e54';
-const API_URL = 'https://www.giantbomb.com/api/games/?api_key=';
+const API_URL = '?api_key=' + API_KEY;
 
-const GAMES = API_URL + API_KEY + `&format=json&sort=original_release_date:desc&filter=original_release_date:${startDate}|${currDate},`;
 
-const SWITCHGAMES = GAMES + "platforms:157,"
+const SWITCHGAMES = "platforms:157,"
 
-async function getGames(query) {
+async function getGame(url) {
+    const query = `${url}?api_key=${API_KEY}&format=json`
+    const res = await fetch(query)
+    const data = await res.json();
+
+    console.log(data.genres.name)
+}
+
+async function getGames(filter) {
+    const games = `https://www.giantbomb.com/api/games/${API_URL}&format=json&sort=original_release_date:desc&filter=original_release_date:${startDate}|${currDate},`;
+    const query = games + filter;
     const res = await fetch(query);
     const data = await res.json();
 
     data.results.forEach(game => {
-        const img = document.createElement('img');
-        img.src = game.image.screen_url;
+        if (game.original_release_date === null) {
+            game.original_release_date = 'Not Listed';
+        }
+        const gameElement = document.createElement('div');
+        gameElement.classList.add('game');
+        gameElement.innerHTML = `
+        <img src="${game.image.screen_url}" alt="${game.deck}"/>
+        <div class="game-desc">
+            <h3>${game.name}</h3>
+            <span>${game.original_release_date}</span>
+        </div>`
 
-        document.body.appendChild(img);
-    })
+        document.body.main.appendChild(gameElement)
+    });
 
     console.log(data);
     return data;
